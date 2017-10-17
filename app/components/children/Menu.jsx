0f39a93dd6c45fc2980a8f075caf2c8c11a526f6
Dustin J Siegel <weenzombie@gmail.com>
2017-10-16 19:22:44 -0600

    connected the Main.jsx page

---
+++ app/components/children/Menu.jsx
@@ -0,0 +1,42 @@
+
+var React = require("react");
+
+var Menu = React.createClass({
+
+  // getInitialState: function() {
+  //   return {
+  //     topic: "",
+  //     startYear: "",
+  //     endYear: ""
+  //   };
+  // },
+  //
+  // _handleSubmit: function(event) {
+  //
+  //   event.preventDefault();
+  //
+  //   this.props._setSearchFeilds(this.state.topic, this.state.startYear, this.state.endYear);
+  // },
+  //
+  // _handleTopicChange: function(e) {
+  //   this.setState({topic: e.target.value});
+  // },
+  //
+  // _handleStartYearChange: function(e) {
+  //   this.setState({startYear: e.target.value});
+  // },
+  //
+  // _handleEndYearChange: function(e) {
+  //   this.setState({endYear: e.target.value});
+  // },
+
+  render: function() {
+    return (
+
+      <div>Some Words in It</div>
+
+    );
+  }
+});
+
+module.exports = Menu;

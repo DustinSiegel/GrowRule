4bf5d37ec54e0f9112f5da3557ccd12385d0f546
dsapkota <dipta.sapkota@viasat.com>
2017-10-18 20:48:42 -0600

    updated files for database functionality

--- app/components/children/Cull.jsx
+++ app/components/children/Cull.jsx
@@ -51,8 +51,8 @@ var Cull = React.createClass({
           <form role="form" onSubmit={this._handleSubmit}>
 
             <div className="form-group col-md-offset-3 col-md-6">
-              <label htmlFor="topic" className="text-center">Enter or Scan PO#(s) to be Culled</label>
-              <input type="text" className="form-control text-center" id="topic" onChange={this._handleTopicChange} />
+              <label htmlFor="Cull" className="text-center">Enter or Scan PO#(s) to be Culled</label>
+              <input type="text" className="form-control text-center" id="cull" onChange={this._handlecullChange} />
               <br />
               <button type="submit" className="btn btn-info btn- col-md-offset-4 col-md-4" id="addField">Add to Change List</button>
             </div>

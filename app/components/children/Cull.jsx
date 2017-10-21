e3e1ddbdb591cf0bab30be16e0d9330e879be939
Dustin J Siegel <weenzombie@gmail.com>
2017-10-20 20:36:22 -0600

    got the fields reading and writing user input

--- app/components/children/Cull.jsx
+++ app/components/children/Cull.jsx
@@ -4,85 +4,71 @@ var React = require("react");
 var Cull = React.createClass({
 
   getInitialState: function() {
-    return {
-      Cull: "",
-    };
-    this.handleCullChange = this.handleCullChange.bind(this)
+    return { text: "" };
   },
 
-  _handleSubmit: function(event) {
-
-    event.preventDefault();
-    alert('it works!');
-
+  handleChange: function(event) {
+    var newState = {};
+    newState[event.target.id] = event.target.value;
+    this.setState(newState);
   },
-
-  _handleCullChange: function(event) {
-    this.setState({Cull: event.target.value})
-    console.log("something is happening!", event.target.value);
-  },
-  //
-  //   this.props._setSearchFeilds(this.state.cull, this.state.poNum, this.state.date);
-  // this.state.strainName);
-  // },
-  //
-  // _handleTopicChange: function(e) {
-  //   this.setState({cull: e.target.value});
-  // },
-  //
-  // _handleStartYearChange: function(e) {
-  //   this.setState({poNum: e.target.value});
-  // },
-  //
-  // _handleEndYearChange: function(e) {
-  //   this.setState({date: e.target.value});
-  // },
-
-  // _handleEndYearChange: function(e) {
-  //   this.setState({strainName: e.target.value});
-  // },
-
-
 
   render: function() {
     return (
 
-      <div className="panel panel-default">
+      <div className="container">
+        <div className="row">
+          <div className="col-xs-12">
+            <div className="panel panel-default">
+              <div className="panel-heading">
+                <h3 className="panel-title text-center">Enter PO#(s) To Be Culled</h3>
+              </div>
+              <div className="panel-body text-center">
+                <form>
+                  <div className="form-group">
 
-        <div className="panel-heading">
-          <h3 className="panel-title text-center" style={ {fontSize: "20px"} }><i><b>Cull</b></i></h3>
-        </div>
-
-        <div className="panel-body text-center">
-          <form role="form" onSubmit={this._handleSubmit}>
-
-            <div className="form-group col-md-offset-3 col-md-6">
-
-              <label htmlFor="Cull" className="text-center">Enter or Scan PO#(s) to be Culled</label>
-              <input type="text" className="form-control text-center" id="cull" onChange={this._handleCullChange} />
-              <br />
-              <button type="submit" className="btn btn-info btn- col-md-offset-4 col-md-4" id="addField">Add to Change List</button>
-
+                    <h4>
+                      <strong>Enter PO#(s) Here:</strong>
+                    </h4>
+                    <input
+                      type="text"
+                      value={this.state.text}
+                      className="form-control"
+                      id="text"
+                      onChange={this.handleChange}
+                      required
+                    />
+                    <br />
+                    <button type="submit" className="btn btn-info btn- col-md-offset-4 col-md-4" id="submitCull">PO Ready</button>
+                  </div>
+                </form>
+              </div>
             </div>
+          </div>
+          <div className="row">
+            <div className="col-xs-12">
+              <div className="panel panel-default">
+                <div className="panel-heading">
+                  <h3 className="panel-title text-center">Plants Ready to Cull:</h3>
+                </div>
+                <div className="panel-body text-center">
+                  <form>
+                    <div className="form-group">
+                      <h2>
+                        {this.state.text.split("").join("")}
+                      </h2>
+                    </div>
+                    <br />
 
-            <br />
-
-            <div className="form-group col-xs-12">
-              <label htmlFor="startYear">Plants Ready to be Culled:</label>
-              <textarea type="text" className="form-control text-center" id="startYear" rows="5" onChange={this._handleStartYearChange} />
+                    <button type="submit" className="btn btn-info col-md-offset-5 col-md-2" id="updateDatabaseBtn">Update Database</button>
+                  </form>
+                </div>
+              </div>
             </div>
-
-            <br />
-
-            <button type="submit" className="btn btn-info col-md-offset-5 col-md-2" id="searchBtn">Update Database</button>
-
-          </form>
+          </div>
         </div>
-
       </div>
-
     );
   }
 });
-
 module.exports = Cull;

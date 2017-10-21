6bcc637c2639e1ba3649aef907e4846b96d0c6d2
ctacderas <ctacderas@gmail.com>
2017-10-21 09:06:44 -0600

    Possible image Heroku fix

--- app/components/children/Menu.jsx
+++ app/components/children/Menu.jsx
@@ -14,12 +14,12 @@ var Menu = React.createClass({
 		        <div className="col col-md-4">
 		        </div>
 		        <div className="col col-md-2 createCloneButton" >
-		          <img src="/images/asset 1@3x.png" height="265px" width="273px" id="createCloneButton" onClick={() => this.props.handleNavClick('Clone')}/>
+		          <img src="images/asset 1@3x.png" height="265px" width="273px" id="createCloneButton" onClick={() => this.props.handleNavClick('Clone')}/>
 		        </div>
 		        <div className="col col-md-2">
 		        </div>
 		        <div className="col col-md-2 movePlantButton">
-		          <img src="/images/asset 2@3x.png" height="269px" width="274px" id="movePlantButton" onClick={() => this.props.handleNavClick('Transfer')}/>
+		          <img src="images/asset 2@3x.png" height="269px" width="274px" id="movePlantButton" onClick={() => this.props.handleNavClick('Transfer')}/>
 		        </div>
 		        <div className="col col-md-4">
 		        </div>
@@ -28,12 +28,12 @@ var Menu = React.createClass({
 		        <div className="col col-md-2">
 		        </div>
 		        <div className="col col-md-2 harvestPlantButton">
-		          <img src="/images/asset 3@3x.png" height="266px" width="270px" id="harvestPlantButton" onClick={() => this.props.handleNavClick('Harvest')}/>
+		          <img src="images/asset 3@3x.png" height="266px" width="270px" id="harvestPlantButton" onClick={() => this.props.handleNavClick('Harvest')}/>
 		        </div>
 		        <div className="col col-md-3">
 		        </div>
 		        <div className="col col-md-2 cullPlantButton">
-		          <img src="/images/asset 4@3x.png" height="269px" width="325px" id="cullPlantButton" onClick={() => this.props.handleNavClick('Cull')}/>
+		          <img src="images/asset 4@3x.png" height="269px" width="325px" id="cullPlantButton" onClick={() => this.props.handleNavClick('Cull')}/>
 		        </div>
 		        <div className="col col-md-3">
 		        </div>
@@ -42,12 +42,12 @@ var Menu = React.createClass({
 		        <div className="col col-md-3">
 		        </div>
 		        <div className="col col-md-2 makeMomButton">
-		          <img src="/images/asset 5@3x.png" height="270px" width="305px" id="makeMomButton" onClick={() => this.props.handleNavClick('Mom')}/>
+		          <img src="images/asset 5@3x.png" height="270px" width="305px" id="makeMomButton" onClick={() => this.props.handleNavClick('Mom')}/>
 		        </div>
 		        <div className="col col-md-1">
 		        </div>
 		        <div className="col col-md-2 viewDataButton">
-		          <img src="/images/asset 7@3x.png" height="280px" width="305px" id="viewDataButton" onClick={() => this.props.handleNavClick('Audit')}/>
+		          <img src="images/asset 7@3x.png" height="280px" width="305px" id="viewDataButton" onClick={() => this.props.handleNavClick('Audit')}/>
 		        </div>
 		        <div className="col col-md-3">
 		        </div>
